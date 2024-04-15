2abfb4298bafe4912c32e8ee50b9201b482d8c7b
upturnist <166921396+upturnist@users.noreply.github.com>
2024-04-15 14:06:01 +0530

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -9,8 +9,9 @@ module.exports = ({ env }) => ({
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
         ssl: {
-          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-        },
+          //rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+          ca: env('DATABASE_CA')
+         },
       },
       debug: false,
     },
